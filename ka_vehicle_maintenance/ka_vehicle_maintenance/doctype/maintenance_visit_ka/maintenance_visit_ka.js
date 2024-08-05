952c39d05b77f245b113bfedc8146df8ffcd2b27
A7medAbdien <ahmed.g.abdien.com>
2024-08-05 11:57:27 +0300

    create doc type maintenance visit and vehicle

---
+++ ka_vehicle_maintenance/ka_vehicle_maintenance/doctype/maintenance_visit_ka/maintenance_visit_ka.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ahmed.g.abdin and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Maintenance Visit KA", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ ka_vehicle_maintenance/ka_vehicle_maintenance/doctype/vehicle_ka/test_vehicle_ka.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ahmed.g.abdin and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVehicleKA(FrappeTestCase):
+	pass
