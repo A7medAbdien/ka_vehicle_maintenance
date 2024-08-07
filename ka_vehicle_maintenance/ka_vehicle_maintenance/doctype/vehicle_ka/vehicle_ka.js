1e85105e9fed4df2e4ba6ca92eea9139b4bc6a2a
A7medAbdien <ahmed.g.abdien.com>
2024-08-07 13:17:09 +0300

    cleaning

--- ka_vehicle_maintenance/ka_vehicle_maintenance/doctype/vehicle_ka/vehicle_ka.js
+++ ka_vehicle_maintenance/ka_vehicle_maintenance/doctype/vehicle_ka/vehicle_ka.js
@@ -3,26 +3,25 @@
 
 frappe.ui.form.on("Vehicle KA", {
     refresh(frm) {
-        // Function to reorder the child table items
-        const reorderChildTable = () => {
-            // Get the child table data
-            let child_table_data = frm.doc.visits;
-
-            // Example: Sort the items alphabetically by a field called 'item_name'
-            child_table_data.sort((a, b) =>
-                a.maintenance_date.localeCompare(b.item_name)
-            );
-
-            // Update the idx values
-            child_table_data.forEach((item, index) => {
-                item.idx = index + 1;
-            });
-
-            // Refresh the field to reflect changes
-            frm.refresh_field("visits");
-        };
-
-        // Call the reorder function (can be triggered by any event or button)
-        reorderChildTable();
+        reorderChildTable(frm);
     },
 });
+
+// Function to reorder the child table items
+const reorderChildTable = (frm) => {
+    // Get the child table data
+    let child_table_data = frm.doc.visits;
+
+    // Example: Sort the items alphabetically by a field called 'item_name'
+    child_table_data.sort((a, b) =>
+        a.maintenance_date.localeCompare(b.item_name)
+    );
+
+    // Update the idx values
+    child_table_data.forEach((item, index) => {
+        item.idx = index + 1;
+    });
+
+    // Refresh the field to reflect changes
+    frm.refresh_field("visits");
+};
