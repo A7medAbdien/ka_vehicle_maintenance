9e13d3ac289778ec428c99007cc24e039016fea5
A7medAbdien <ahmed.g.abdien.com>
2024-08-13 19:19:31 +0300

    init vehicle report

--- ka_vehicle_maintenance/ka_vehicle_maintenance/report/vehicle_report/vehicle_report.py
+++ ka_vehicle_maintenance/ka_vehicle_maintenance/report/vehicle_report/vehicle_report.py
@@ -4,6 +4,56 @@
 # import frappe
 
 
+import frappe
+
+
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns, data = [], []
+
+    columns = [
+        {
+            "fieldname": "name",
+            "label": "Vehicle",
+            "fieldtype": "Link",
+            "options": "Vehicle KA",
+        },
+        {
+            "fieldname": "state",
+            "label": "Status",
+            "fieldtype": "Data",
+        },
+        {
+            "fieldname": "current_km",
+            "label": "Current KM",
+            "fieldtype": "Float",
+        },
+        {
+            "fieldname": "last_visit_date",
+            "label": "Last Visit",
+            "fieldtype": "Date",
+        },
+    ]
+
+    vehicles = get_vehicle(filters)
+    data = [
+        {
+            "name": row.name,
+            "state": row.state,
+            "current_km": row.current_km,
+            "last_visit_date": row.last_visit_date,
+        }
+        for row in vehicles
+    ]
+    return columns, data
+
+
+def get_vehicle(filters):
+    v = frappe.qb.DocType("Vehicle KA")
+
+    query = frappe.qb.from_(v).select(
+        v.name,
+        v.state,
+        v.current_km,
+        v.last_visit_date,
+    )
+    return query.run(as_dict=True)
